8f0b7ec2052e8eb0e8af7e55784dc96806ef9ed6
marcus-learns-things <marcusmoorejr1230@gmail.com>
2024-10-22 10:13:07 -0500

    Restructured for easier viewing

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'some_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+import routes
